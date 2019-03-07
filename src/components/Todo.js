35cbf344aca1ba5afd3a0aac93ce606aab2cfc51
Allan Tang <tang.allan03@gmail.com>
2019-03-06 22:34:33 -0800

    Messing with react hooks

---
+++ src/components/Todo.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const todo = (props) => {
+    const [todoName, setTodoName] = useState('');
+    const [todoList, setTodoList] = useState([]);
+
+    // hooks with react internals
+    // infiite loop? - runs after every render cycle
+    useEffect(() => {
+        axios.get('https://practice-project-6130e.firebaseio.com/todos.json')
+            .then(res => {
+                console.log(res);
+                const todoData = res.data;
+                let todos = [];
+                for(const key in todoData) {
+                    todos.push({ id: key, name: todoData[key].name });
+                }
+
+                setTodoList(todos);
+            });
+    }, []); //if used [todoName], the use effect function will run whenever todoName is changed
+    // if provided by [], it runs only once acting like componentDidMount()
+
+    // NO AUTOMATIC MERGE
+    // const [todoState, setTodoState] = useState({
+    //     userInput: '',
+    //     todoList: []
+    // });
+
+    const onTodoNameHandler = (evt) => {
+        setTodoName(evt.target.value);
+        // setTodoState({
+        //     ...todoState,
+        //     userInput: evt.target.value
+        // });
+    };
+
+    const todoAddHandler = () => {
+        setTodoList([...todoList, todoName]);
+        setTodoName('');
+        // setTodoState({
+        //     ...todoState,
+        //     userInput: '',
+        // //     todoList: [...todoState.todoList, todoState.userInput]
+        // });
+        axios.post('https://practice-project-6130e.firebaseio.com/todos.json', { name: todoName })
+            .then(res => {
+                console.log(res);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    };
+
+    return <React.Fragment>
+        <input type="text" placeholder="Enter Todo" onChange={onTodoNameHandler} value={todoName} />
+        <button type="button" onClick={todoAddHandler}>Add</button>
+
+        <ul>
+            {todoList.map((m,i) => <li key={m.id}>{m.name}</li>)}
+        </ul>
+    </React.Fragment>
+};
+
+
+export default todo;
